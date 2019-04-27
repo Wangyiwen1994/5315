ac11bc8c76bee22306bc49ed42d995c8dd3ccab1
leah <wangyiwen@seattleu.edu>
2019-04-26 22:12:22 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -9,116 +9,116 @@
       "Item_ID": "1022"
     },
     {
-      "item_name": "foundation",
-      "category": "makeup",
-      "price": "$75",
+      "Item_name": "foundation",
+      "Category": "makeup",
+      "Price": "$75",
       "stock_date": "2019/01/02",
-      "store": "Sweetie",
-      "item_ID": "1004"
+      "Store": "Sweetie",
+      "Item_ID": "1004"
     },
     {
-      "item_name": "denim skirt",
-      "category": "clothes",
-      "price": "$186",
+      "Item_name": "denim skirt",
+      "Category": "clothes",
+      "Price": "$186",
       "stock_date": "2019/01/12",
-      "store": "LeahFashionClub",
-      "item_ID": "1562"
+      "Store": "LeahFashionClub",
+      "Item_ID": "1562"
     },
     {
-      "item_name": "sneaker",
-      "category": "clothes",
-      "price": "$162",
+      "Item_name": "sneaker",
+      "Category": "clothes",
+      "Price": "$162",
       "stock_date": "2019/04/11",
-      "store": "LeahFashionClub",
-      "item_ID": "1119"
+      "Store": "LeahFashionClub",
+      "Item_ID": "1119"
     },
     {
-      "item_name": "makeup cleaner",
-      "category": "makeup",
-      "price": "$42",
+      "Item_name": "makeup cleaner",
+      "Category": "makeup",
+      "Price": "$42",
       "stock_date": "2019/02/12",
-      "store": "Sweetie",
-      "item_ID": "1330"
+      "Store": "Sweetie",
+      "Item_ID": "1330"
     },
     {
-      "item_name": "hat",
-      "category": "clothes",
-      "price": "$84",
+      "Item_name": "hat",
+      "Category": "clothes",
+      "Price": "$84",
       "stock_date": "2018/11/13",
-      "store": "LeahFashionClub",
-      "item_ID": "1373"
+      "Store": "LeahFashionClub",
+      "Item_ID": "1373"
     },
     {
-      "item_name": "eye liner",
-      "category": "makeup",
-      "price": "$18",
+      "Item_name": "eye liner",
+      "Category": "makeup",
+      "Price": "$18",
       "stock_date": "2018/12/29",
-      "store": "Sweetie",
-      "item_ID": "1224"
+      "Store": "Sweetie",
+      "Item_ID": "1224"
     },
     {
-      "item_name": "socks",
-      "category": "clothes",
-      "price": "$10",
+      "Item_name": "socks",
+      "Category": "clothes",
+      "Price": "$10",
       "stock_date": "2019/02/18",
-      "store": "LeahFashionClub",
-      "item_ID": "1597"
+      "Store": "LeahFashionClub",
+      "Item_ID": "1597"
     },
     {
-      "item_name": "lipstick",
-      "category": "makeup",
-      "price": "$45",
+      "Item_name": "lipstick",
+      "Category": "makeup",
+      "Price": "$45",
       "stock_date": "2019/03/15",
-      "store": "Sweetie",
-      "item_ID": "1360"
+      "Store": "Sweetie",
+      "Item_ID": "1360"
     },
     {
-      "item_name": "jeans",
-      "category": "clothes",
-      "price": "$217",
+      "Item_name": "jeans",
+      "Category": "clothes",
+      "Price": "$217",
       "stock_date": "2018/09/26",
-      "store": "LeahFashionClub",
-      "item_ID": "1290"
+      "Store": "LeahFashionClub",
+      "Item_ID": "1290"
     },
     {
-      "item_name": "blusher",
-      "category": "makeup",
-      "price": "$35",
+      "Item_name": "blusher",
+      "Category": "makeup",
+      "Price": "$35",
       "stock_date": "2019/02/06",
-      "store": "Sweetie",
-      "item_ID": "1608"
+      "Store": "Sweetie",
+      "Item_ID": "1608"
     },
     {
-      "item_name": "T-shirt",
-      "category": "clothes",
-      "price": "$109",
+      "Item_name": "T-shirt",
+      "Category": "clothes",
+      "Price": "$109",
       "stock_date": "2018/10/14",
-      "store": "LeahFashionClub",
-      "item_ID": "1200"
+      "Store": "LeahFashionClub",
+      "Item_ID": "1200"
     },
     {
-      "item_name": "foundation",
-      "category": "makeup",
-      "price": "$75",
+      "Item_name": "foundation",
+      "Category": "makeup",
+      "Price": "$75",
       "stock_date": "2018/12/13",
-      "store": "Sweetie",
-      "item_ID": "0667"
+      "Store": "Sweetie",
+      "Item_ID": "0667"
     },
     {
-      "item_name": "denim skirt",
-      "category": "clothes",
-      "price": "$186",
+      "Item_name": "denim skirt",
+      "Category": "clothes",
+      "Price": "$186",
       "stock_date": "2018/12/19",
-      "store": "LeahFashionClub",
-      "item_ID": "0814"
+      "Store": "LeahFashionClub",
+      "Item_ID": "0814"
     },
     {
-      "item_name": "essence",
-      "category": "makeup",
-      "price": "$342",
+      "Item_name": "essence",
+      "Category": "makeup",
+      "Price": "$342",
       "stock_date": "2019/03/03",
-      "store": "Sweetie",
-      "item_ID": "1497"
+      "Store": "Sweetie",
+      "Item_ID": "1497"
     }
   ]
 }
